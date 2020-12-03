a4868404488d9616491cdc4c6cd773f28106e3f6
Adam Hägglund <adm.hagglund@gmail.com>
2020-12-03 10:49:50 +0100

    Removing exception as suggested in review.

--- ETSNLab4/src/Main.java
+++ ETSNLab4/src/Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 	
 		
 	if(args[0].equals("search") && args.length == 3) {
